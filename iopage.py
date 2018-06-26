0abf44fcc60eeaa21525b84745b23c10028d6b43
Yu Yu Lwin <yulwin178@gmail.com>
2018-06-26 10:56:06 +0630

    added iopage.py

---
+++ iopage.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, url_for
+
+myapp = Flask(__name__)
+
+@myapp.route("/")
+def hello():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    myapp.run(debug = True)
